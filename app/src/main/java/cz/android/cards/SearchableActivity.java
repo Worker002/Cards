815d2ec925e10c9891bce6a44cdf5f33eeaa75f6
Worker002 <dominikmach@outlook.com>
2017-11-12 16:24:05 +0100

    General update

--- app/src/main/java/cz/android/cards/SearchableActivity.java
+++ app/src/main/java/cz/android/cards/SearchableActivity.java
@@ -1,22 +1,34 @@
 package cz.android.cards;
 
 import android.app.SearchManager;
-import android.content.ComponentName;
-import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 
 /**
  * Created by dominikmach on 09/11/2017.
  */
 
 public class SearchableActivity {
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu() {
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         // Can be replaced with getComponentName()
         // if this searchable activity is the current activity
         ComponentName componentName = new ComponentName(context, SearchableActivity.class);
         searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName));
+*/
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
 
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
 
         //getMenuInflater().inflate(R.menu.main_activity_actions, menu);
         //MenuItem searchItem = menu.findItem(R.id.action_search);
